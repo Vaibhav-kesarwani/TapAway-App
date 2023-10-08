247538ddeb42d18528f28206ec53b1467f786082
vaibhav kesarwani <vaibhavkesarwani100@gmail.com>
2023-10-08 19:11:50 +0530

    added_the_drawer

---
+++ lib/screens/my_header_drawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MyHeaderDrawer extends StatefulWidget {
+  const MyHeaderDrawer({super.key});
+
+  @override
+  State<MyHeaderDrawer> createState() => _MyHeaderDrawerState();
+}
+
+class _MyHeaderDrawerState extends State<MyHeaderDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green[700],
+      width: double.infinity,
+      height: 200,
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            height: 70,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/image1.png'),
+              ),
+            ),
+          ),
+          const Text(
+            "Vaibhav Kesarwani",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "abc@gmail.com",
+            style: TextStyle(
+              color: Colors.grey[200],
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
