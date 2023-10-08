2dbe40c7302b54d15e0dc6d55cd91fc8bec547e6
vaibhav kesarwani <vaibhavkesarwani100@gmail.com>
2023-10-08 11:43:15 +0530

    Added_the_map

--- lib/screens/map_screen/map_screen.dart
+++ lib/screens/map_screen/map_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uhack_app/screens/home_screen.dart';
+
+const LatLng currentLocation = LatLng(25.1193, 55.3773);
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,6 +12,11 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  // ignore: unused_field
+  late GoogleMapController _mapController;
+  // ignore: prefer_final_fields
+  Map<String, Marker> _markers = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +41,31 @@ class _MapScreenState extends State<MapScreen> {
           ),
         ),
       ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: currentLocation,
+          zoom: 14,
+        ),
+        onMapCreated: (controller) {
+          _mapController = controller;
+          addMarker('test', currentLocation);
+        },
+        markers: _markers.values.toSet(),
+      ),
     );
+  }
+
+  addMarker(String id, LatLng location) {
+    var marker = Marker(
+      markerId: MarkerId(id),
+      position: location,
+      infoWindow: const InfoWindow(
+        title: 'Title of the place',
+        snippet: 'Some description of the place',
+      ),
+    );
+
+    _markers[id] = marker;
+    setState(() {});
   }
 }
