6dc88d54c5300938d236b4d657a55ee445ae968a
Vaibhav Kesarwani <vaibhavkesarwani100@gmail.com>
2023-10-19 20:01:49 +0530

    chnage_the_app_name_and_logo

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: WelcomeScreen(),
-        title: "U_Hack_Application",
+        title: "tapAway",
       ),
     );
   }
